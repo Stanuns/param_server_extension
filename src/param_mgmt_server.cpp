d02c15b360c85efeacea39ba85bb56e4118c4e8b
wei.sun <sunweijm.sun@gmail.com>
2025-05-15 14:36:46 +0800

    add service function

--- src/param_mgmt_server.cpp
+++ src/param_mgmt_server.cpp
@@ -42,21 +42,7 @@ private:
 
             switch (request->cmd_name) {
               case 1:  // Version
-                  try{
-                      ;
-                  }catch (const std::exception& e) {
-                      response->err_code = 500;
-                      response->err_msg = std::string("Error in Get Version param: ") + e.what();
-                      RCLCPP_ERROR(get_logger(), "Get Version param error: %s", e.what());
-                      break;
-                  } catch (...) {
-                      response->err_code = 500;
-                      response->err_msg = "Unknown error in Get Version param";
-                      RCLCPP_ERROR(get_logger(), "Unknown error in Get Version param");
-                      break;
-                  }
-                  response->err_code = 200;
-                  response->err_msg = std::string("Get Version param successfully");
+                  get_chassis_sw_version(response);    
                   break;
               case 2:  // Speed
                   get_max_speed_param(response);
@@ -91,16 +77,7 @@ private:
                   response->err_msg = "Get Charge Threshold param successfully";
                   break;
               case 5:  // Battery level
-                  try {
-                      ;
-                  } catch (const std::exception& e) {
-                      response->err_code = 500;
-                      response->err_msg = std::string("Error in Get Battery Level param: ") + e.what();
-                      RCLCPP_ERROR(get_logger(), "Get Battery Level param error: %s", e.what());
-                      break;
-                  }
-                  response->err_code = 200;
-                  response->err_msg = "Get Battery Level param successfully";
+                  get_battery_level(response);  
                   break;
               default:
                   response->err_code = 500;
@@ -126,6 +103,68 @@ private:
 
     }
 
+    void get_battery_level(std::shared_ptr<robot_interfaces::srv::ParamServer::Response> response){
+        RCLCPP_INFO(this->get_logger(), "start to get_battery_level");
+        auto node = rclcpp::Node::make_shared("temp_get_param_node");
+        
+        // Declare parameter client
+        auto param_client = std::make_shared<rclcpp::SyncParametersClient>(node, "/hm_base_driver_node");
+        
+        // Wait for service to be available
+        if (!param_client->wait_for_service(std::chrono::seconds(1))) {
+            response->err_code = 500;
+            response->err_msg = "Parameter service not available";
+            return;
+        }
+
+        try {
+            // Get the parameter value
+            auto b_level = param_client->get_parameter<float>("battery_level");
+            response->battery_level = b_level;
+            response->err_code = 200;
+            response->err_msg = "Get battery level param successfully";
+        } catch (const rclcpp::exceptions::ParameterNotDeclaredException & e) {
+            response->err_code = 501;
+            response->err_msg = "Parameter battery_level not declared";
+        } catch (const std::exception & e) {
+            response->err_code = 502;
+            response->err_msg = std::string("Error getting battery level param: ") + e.what();
+        }
+    }
+
+    void get_chassis_sw_version(std::shared_ptr<robot_interfaces::srv::ParamServer::Response> response){
+        RCLCPP_INFO(this->get_logger(), "start to get_chassis_sw_version");
+        auto node = rclcpp::Node::make_shared("temp_get_param_node");
+        
+        // Declare parameter client
+        auto param_client = std::make_shared<rclcpp::SyncParametersClient>(node, "/hm_base_driver_node");
+        
+        // Wait for service to be available
+        if (!param_client->wait_for_service(std::chrono::seconds(1))) {
+            response->err_code = 500;
+            response->err_msg = "Parameter service not available";
+            return;
+        }
+
+        try {
+            // Get the parameter value
+            auto v_mcu = param_client->get_parameter<string>("version_mcu");
+            auto v_main_control_sw = param_client->get_parameter<string>("version_main_control_sw");
+            response->version_list.clear();
+            response->version_list.push_back(v_mcu);
+            response->version_list.push_back(v_main_control_sw);
+            response->err_code = 200;
+            response->err_msg = "Get sw version param successfully";
+        } catch (const rclcpp::exceptions::ParameterNotDeclaredException & e) {
+            response->err_code = 501;
+            response->err_msg = "Parameter version_mcu or version_main_control_sw not declared";
+        } catch (const std::exception & e) {
+            response->err_code = 502;
+            response->err_msg = std::string("Error getting sw version param: ") + e.what();
+        }
+
+    }
+
     void get_max_speed_param(std::shared_ptr<robot_interfaces::srv::ParamServer::Response> response) {
         RCLCPP_INFO(this->get_logger(), "start to get_max_speed_param");
 
@@ -143,18 +182,19 @@ private:
 
         try {
             // Get the parameter value
-            auto max_speed = param_client->get_parameter<float>("max_linear_speed");
-            
+            auto max_linear = param_client->get_parameter<float>("max_linear_speed");
+            auto max_angular = param_client->get_parameter<float>("max_angular_speed");
             response->speed_list.clear();
-            response->speed_list.push_back(max_speed);
+            response->speed_list.push_back(max_linear);
+            response->speed_list.push_back(max_angular);
             response->err_code = 200;
-            response->err_msg = "Get Speed param successfully";
+            response->err_msg = "Get max speed param successfully";
         } catch (const rclcpp::exceptions::ParameterNotDeclaredException & e) {
             response->err_code = 501;
-            response->err_msg = "Parameter max_linear_speed not declared";
+            response->err_msg = "Parameter max_linear_speed or max_angular_speed not declared";
         } catch (const std::exception & e) {
             response->err_code = 502;
-            response->err_msg = std::string("Error Getting Speed param: ") + e.what();
+            response->err_msg = std::string("Error getting max speed param: ") + e.what();
         }
     }
 
@@ -164,26 +204,28 @@ private:
 
         //Set parameters
         if (request->speed_list.empty()) {
-            response->err_code = 1;
+            response->err_code = 500;
             response->err_msg = "Request speed_list is empty";
             return;
         }
-        float max_speed = request->speed_list[0];
+        float max_linear_speed = request->speed_list[0];
+        float max_angular_speed = request->speed_list[1];
         // Create a parameter client for the target node
         auto param_client = std::make_shared<rclcpp::SyncParametersClient>(
             node,
             "hm_base_driver_node");
         if (!param_client->wait_for_service(std::chrono::seconds(1))) {
-            response->err_code = 500;
+            response->err_code = 501;
             response->err_msg = "node /hm_base_driver_node Parameter service not available";
             return;
         }
         // Set the parameter
         auto result = param_client->set_parameters({
-            rclcpp::Parameter("max_linear_speed", max_speed)
+            rclcpp::Parameter("max_linear_speed", max_linear_speed),
+            rclcpp::Parameter("max_angular_speed", max_angular_speed)
         });
         if (!result[0].successful) {
-            response->err_code = 501;
+            response->err_code = 502;
             response->err_msg = "Failed to set parameter";
             return;
         }
@@ -205,7 +247,7 @@ private:
         // Add nullptr check for 'this'
         if (!this) {
             RCLCPP_ERROR(rclcpp::get_logger("param_mgmt_server"), "Invalid object state!");
-            response->err_code = 507;
+            response->err_code = 503;
             response->err_msg = "Internal server error";
             return;
         }    
@@ -221,13 +263,13 @@ private:
             node_names = this->get_node_names();
         } catch (const std::exception& e) {
             RCLCPP_ERROR(this->get_logger(), "Failed to get node names: %s", e.what());
-            response->err_code = 508;
+            response->err_code = 504;
             response->err_msg = "Failed to query node names";
             return;
         }
         if (std::find(node_names.begin(), node_names.end(), node_name) == node_names.end()) {
             RCLCPP_ERROR(this->get_logger(), "Node '%s' not found!", node_name.c_str());
-            response->err_code = 502;
+            response->err_code = 505;
             response->err_msg = std::string("Node ")+ node_name + std::string("not found!");
             return;
         }
@@ -241,7 +283,7 @@ private:
         // List parameters
         if (!list_client->wait_for_service(3s)) {
             RCLCPP_ERROR(this->get_logger(), "Dump parameters, list_parameters service unavailable");
-            response->err_code = 503;
+            response->err_code = 506;
             response->err_msg = "Dump parameters, list_parameters service unavailable";
             return;
         }
@@ -253,7 +295,7 @@ private:
         if (executor->spin_until_future_complete(list_future, 3s) != 
             rclcpp::FutureReturnCode::SUCCESS) {
             RCLCPP_ERROR(this->get_logger(), "Dump parameters, Failed to list parameters");
-            response->err_code = 504;
+            response->err_code = 507;
             response->err_msg = "Dump parameters, Failed to list parameters";
             executor->remove_node(node_dump_param);
             return;
@@ -263,7 +305,7 @@ private:
         executor->remove_node(node_dump_param);
         if (param_names.empty()) {
             RCLCPP_WARN(this->get_logger(), "Dump parameters, No parameters found for %s", node_name.c_str());
-            response->err_code = 505;
+            response->err_code = 508;
             response->err_msg = std::string("Dump parameters, No parameters found for ") + node_name;
             return;
         }
@@ -295,7 +337,7 @@ private:
         }
         
         executor->remove_node(node_dump_param);
-        response->err_code = 506;
+        response->err_code = 509;
         response->err_msg = "Dump parameters, Cannot get param_values";
         // node_param.reset();
         RCLCPP_ERROR(get_logger(), "Dump parameters error, Cannot get param_values");
